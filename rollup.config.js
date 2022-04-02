2767ac03c089df68e825310a87332c357d7fe0c6
pengyw97 <877285030@qq.com>
2022-04-02 17:52:50 +0800

    chore: add rollup plugin @rollup/plugin-node-resolve @rollup/plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "rollup"
-import babel from "rollup-plugin-babel"
+import babel from "rollup-plugin-babel" 
+import commonjs from "@rollup/plugin-commonjs"
+import nodeResolve from "@rollup/plugin-node-resolve"
 
 export default defineConfig([
   // Commonjs
@@ -10,6 +12,8 @@ export default defineConfig([
       format: 'cjs', // 打包文件输出格式（commonjs是Nodejs的官方模块化规范）
     }, 
     plugins: [ // 使用插件
+      nodeResolve(), // 解析node_modules中的模块
+      commonjs(), // 将commonjs模块转换为ES模块
       babel({
         exclude: 'node_modules/**', // 忽略node_modules文件夹
       })
