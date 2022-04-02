5b31310cb41d966249998152db9780944b4d8799
pengyw97 <877285030@qq.com>
2022-04-02 16:28:24 +0800

    chore: rollup & gitignore

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
-export default {
-  input: './src/index.js', // 打包文件入口
-  output: {
-    file: 'dist/toy-redux.cjs.js', // 打包输出文件
-    format: 'cjs', // 打包文件输出格式（commonjs是Nodejs的官方模块化规范）
+import { defineConfig } from "rollup"
+
+export default defineConfig([
+  // Commonjs
+  {
+    input: './src/index.js', // 打包文件入口
+    output: {
+      file: 'lib/toy-redux.js', // 打包输出文件
+      format: 'cjs', // 打包文件输出格式（commonjs是Nodejs的官方模块化规范）
+    }
   },
-}
+])
