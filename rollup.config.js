545d02a79245727984b4cbfe269f583549341fc3
pengyw97 <877285030@qq.com>
2022-04-02 16:31:45 +0800

    chore: update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "rollup"
+import babel from "rollup-plugin-babel"
 
 export default defineConfig([
   // Commonjs
@@ -7,6 +8,11 @@ export default defineConfig([
     output: {
       file: 'lib/toy-redux.js', // 打包输出文件
       format: 'cjs', // 打包文件输出格式（commonjs是Nodejs的官方模块化规范）
-    }
+    }, 
+    plugins: [ // 使用插件
+      babel({
+        excludes: 'node_modules/**', // 忽略node_modules文件夹
+      })
+    ]
   },
 ])
