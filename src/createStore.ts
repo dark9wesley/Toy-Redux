31439b00196be2343088f1f2a4bccb40d95c6f0b
pengyw97 <877285030@qq.com>
2022-05-22 23:57:06 +0800

    feat: createStore 30%

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,5 +1,69 @@
 function createStore(reducer: any, preloadedState: any, enhancer: any){
-  console.log('I am createStore')
+  let state = preloadedState;
+  let currentReducer = reducer;
+  let currentListener = [];
+  let nextListener = currentListener;
+  let isDispatching = false;
+
+  const ensureLister = () => {
+    if(currentListener === nextListener){
+      nextListener = currentListener.slice()
+    }
+  }
+
+  const getStates = () => {
+    return state
+  }
+
+  const dispatch = (action) => {
+    if(typeof action.type === 'undefined'){
+      throw new Error('action must have a type ')
+    }
+
+    try{
+      isDispatching = true
+      state =  currentReducer(state, action)
+    }finally{
+      isDispatching = false
+    }
+
+    const listener = (currentListener = nextListener)
+    for(let i = 0; i < listener.length; i++){
+      listener[i]()
+    }
+  }
+
+  const subScribe = (listener) => {
+    if(typeof listener !== 'function'){
+      throw new Error('listener must be a function')
+    }
+    
+    let isSubscribe = true
+
+    ensureLister()
+
+    nextListener.push(listener)
+
+    return () => {
+      if(!isSubscribe) return
+
+      ensureLister()
+
+      let index = nextListener.indexOf(listener)
+      nextListener.splice(index, 1)
+    }
+  }
+
+  const replaceReducer = () => {
+
+  }
+
+  return {
+    getStates, 
+    dispatch,
+    subScribe,
+    replaceReducer,
+  }
 }
 
 export default createStore
